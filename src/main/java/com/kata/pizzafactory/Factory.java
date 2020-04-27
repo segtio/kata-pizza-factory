60982bd08e0e3e1e92f0674a94bf011fc6942b91
Sega Diop <sega.diop@madi-corp.net>
2020-04-27 11:29:35 +0200

    Prepare 4 seasons Pizza with minimum 1 Topping (KO)

--- src/main/java/com/kata/pizzafactory/Factory.java
+++ src/main/java/com/kata/pizzafactory/Factory.java
@@ -1,14 +1,24 @@
 package com.kata.pizzafactory;
 
+import com.kata.pizzafactory.exception.InvalidRecipe;
+
 import java.util.List;
 
 public class Factory {
     public static Pizza prepare(String pizzaType, String size, List<String> toppings, double price) {
+        if (!checkToppings(toppings)) {
+            throw new InvalidRecipe();
+        }
+
         return Pizza.builder()
                 .size(size)
                 .toppings(toppings)
                 .type(pizzaType)
                 .price(price)
                 .build();
+    }
+
+    static boolean checkToppings(List<String> toppings) {
+        return toppings != null && toppings.size() > 0;
     }
 }
